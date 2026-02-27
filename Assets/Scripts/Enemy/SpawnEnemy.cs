ab3170d45ad0f7a5563f1ef6c4671ce15b3cd7ee
pappmate25 <pappmate22@gmail.com>
2026-02-27 18:44:43 +0100

    set up enemy spawner

---
+++ Assets/Scripts/Enemy/SpawnEnemy.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    [SerializeField] private GameObject enemyPrefab;
+    private float spawnDelay = 0.3f;
+    private int maxEnemy = 100;
+
+
+
+}
