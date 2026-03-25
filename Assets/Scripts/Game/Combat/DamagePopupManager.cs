5814d44316796bd22614d87b4f97ecc118542ce0
pappmate25 <pappmate22@gmail.com>
2026-03-25 14:25:01 +0100

    damage popup delegate bug fix

--- Assets/Scripts/Game/Combat/DamagePopupManager.cs
+++ Assets/Scripts/Game/Combat/DamagePopupManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,8 +11,12 @@ public class DamagePopupManager : MonoBehaviour
 
     private Queue<DamagePopup> pool = new Queue<DamagePopup>();
 
+    private Action<(Vector3, int)> enemyHitHandler;
+
     private void Awake()
     {
+        enemyHitHandler = data => ShowPopup(data.Item1, data.Item2);
+
         for (int i = 0; i < poolSize; i++)
         {
             DamagePopup popup = Instantiate(popupPrefab, transform);
@@ -22,12 +27,12 @@ public class DamagePopupManager : MonoBehaviour
 
     private void OnEnable()
     {
-        onEnemyHit.Subscribe(data => ShowPopup(data.Item1, data.Item2));
+        onEnemyHit.Subscribe(enemyHitHandler);
     }
 
     private void OnDisable()
     {
-        onEnemyHit.UnSubscribe(data => ShowPopup(data.Item1, data.Item2));
+        onEnemyHit.UnSubscribe(enemyHitHandler);
     }
 
     public void ShowPopup(Vector3 position, int damage)
