387f315e7eb39d3836d1d9cb04a7086ab6028734
pappmate25 <pappmate22@gmail.com>
2026-03-26 14:07:23 +0100

    new DungeonDoor

--- Assets/Scripts/Game/Dungeon/DungeonDoor.cs
+++ Assets/Scripts/Game/Dungeon/DungeonDoor.cs
@@ -1,23 +1,56 @@
 using System.Runtime.CompilerServices;
+using Unity.Cinemachine;
 using UnityEngine;
+using static Unity.Collections.AllocatorManager;
 
 public class DungeonDoor : MonoBehaviour
 {
     [SerializeField] private int targetRoomIndex;
+    [SerializeField] private GameEventSO RoomClearedEvent;
+    [SerializeField] private SpriteRenderer doorVisuals;
+    [SerializeField] private Color lockedColor = Color.red;
+    [SerializeField] private Color unlockedColor = Color.green;
 
     private DungeonManager dungeonManager;
+    private bool isUnlocked;
 
     private void Awake()
     {
         dungeonManager = FindFirstObjectByType<DungeonManager>();
     }
 
+    private void OnEnable()
+    {
+        isUnlocked = false;
+        UpdateVisual();
+        RoomClearedEvent.Subscribe(Unlock);
+    }
+
+    private void OnDisable()
+    {
+        RoomClearedEvent.UnSubscribe(Unlock);
+    }
+
+    private void Unlock()
+    {
+        isUnlocked = true;
+        UpdateVisual();
+    }
+
+    private void UpdateVisual()
+    {
+        if(doorVisuals != null)
+        {
+            doorVisuals.color = isUnlocked ? lockedColor : unlockedColor;
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        // if (room != cleared) return;
+        if (!isUnlocked) return;
 
         int layerIndex = LayerMask.NameToLayer("Player");
-        if (other.gameObject.layer == layerIndex)
+        if(other.gameObject.layer == layerIndex)
         {
             dungeonManager.LoadRoom(targetRoomIndex);
         }
