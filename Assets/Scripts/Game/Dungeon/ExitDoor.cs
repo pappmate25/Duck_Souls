82060401f0bccec395bb50eeebed9559458b3f05
pappmate25 <pappmate22@gmail.com>
2026-03-26 14:12:33 +0100

    new ExitDoor

--- Assets/Scripts/Game/Dungeon/ExitDoor.cs
+++ Assets/Scripts/Game/Dungeon/ExitDoor.cs
@@ -3,15 +3,50 @@ using UnityEngine;
 
 public class ExitDoor : MonoBehaviour
 {
-    public static Action OnReturnToHub;
+    [SerializeField] private GameEventSO ExitDoor_onReturnToHub;
+    [SerializeField] private GameEventSO RoomClearedEvent;
+    [SerializeField] private SpriteRenderer doorVisual;
+    [SerializeField] private Color lockedColor = Color.red;
+    [SerializeField] private Color unlockedColor = Color.green;
+
+    private bool isUnlocked;
+
+
+    private void OnEnable()
+    {
+        isUnlocked = false;
+        UpdateVisual();
+        RoomClearedEvent.Subscribe(Unlock);
+    }
+
+    private void OnDisable()
+    {
+        RoomClearedEvent.UnSubscribe(Unlock);
+    }
+
+    private void Unlock()
+    {
+        isUnlocked = true;
+        UpdateVisual();
+    }
+
+    private void UpdateVisual()
+    {
+        if(doorVisual != null)
+        {
+            doorVisual.color = isUnlocked ? lockedColor : unlockedColor;
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (!isUnlocked) return;
+
         int layerIndex = LayerMask.NameToLayer("Player");
 
         if(other.gameObject.layer == layerIndex)
         {
-            OnReturnToHub?.Invoke();
+            ExitDoor_onReturnToHub.Invoke();
         }
     }
 }
